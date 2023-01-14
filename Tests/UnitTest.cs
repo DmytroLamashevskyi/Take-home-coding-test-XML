bc47de188f8dde3fe7661d842c9d844862ef6d39
Lamashevskyi <d.lamashevskyi@gmail.com>
2023-01-14 17:13:20 +0100

    Init Tests Created inout check

--- Tests/UnitTest.cs
+++ Tests/UnitTest.cs
@@ -1,16 +1,19 @@
+using XMLValidator;
+
 namespace Tests
 {
     public class UnitTest
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
+    {    
         [Test]
-        public void Test1()
+        [TestCase("", ExpectedResult = false)] 
+        [TestCase("<Design><Code>hello world</Code></Design>",ExpectedResult = true)]
+        [TestCase("<Design><Code>hello world</Code></Design><People>", ExpectedResult = false)]
+        [TestCase("<People><Design><Code>hello world</People></Code></Design>", ExpectedResult = false)]
+        [TestCase("<People age=”1”>hello world</People>", ExpectedResult = false)]
+        public bool ValidatorTest(string input)
         {
-            Assert.Pass();
+            IXmlValidator validator = new XmlValidator();
+            return validator.IsValid(input);
         }
     }
 }
---
+++ XMLValidator/XmlValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace XMLValidator
+{
+    public interface IXmlValidator
+    {
+       bool IsValid(string str);
+    }
+
+    public class XmlValidator : IXmlValidator
+    {
+        public bool IsValid(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return false;
+            }
+
+
+
+
+        }
+    }
+}
