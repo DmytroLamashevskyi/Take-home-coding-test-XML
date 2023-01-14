5e0273c1ea327935a153c0a5bbeeb6a1d0f98d11
Lamashevskyi <d.lamashevskyi@gmail.com>
2023-01-14 17:00:32 +0100

    Add project files.

---
+++ Tests/UnitTest.cs
@@ -0,0 +1,16 @@
+namespace Tests
+{
+    public class UnitTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
